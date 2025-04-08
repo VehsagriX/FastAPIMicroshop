34a0dc82c39d08242163d431abadba554f9f3135
Khasan <khasan.irgashev.97@gmail.com>
2025-04-08 14:09:16 +0500

    create relationships user post

--- core/models/post.py
+++ core/models/post.py
@@ -1,8 +1,13 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 from .base import Base
+
+# Это делается, чтоб избежать ошибки циклических импортов
+if TYPE_CHECKING:  # Если сейчас проверка типов, то импортируй
+    from .user import User
 
 
 class Post(Base):
@@ -14,3 +19,4 @@ class Post(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"),
     )
+    user: Mapped["User"] = relationship("User", back_populates="posts")
